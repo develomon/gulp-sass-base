d8c810026d3e099d980a0ae9e6d2ee3b301f1fe0
MacMladen iMac <mladen@bluefish.rs>
2015-09-25 19:03:58 +0200

    Added better gulpfile.js inspired by @HugoGiraudel and added his sass-boilerplate.

--- gulpfile.js
+++ gulpfile.js
@@ -1,58 +1,112 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var sourcemaps  = require('gulp-sourcemaps');
-var importOnce  = require('node-sass-import-once');
-var cssGlobbing = require('gulp-css-globbing');
-var assetFunctions = require('node-sass-asset-functions');
+'use strict';
+
+
+// -----------------------------------------------------------------------------
+// Dependencies
+// -----------------------------------------------------------------------------
+
+var gulp         = require('gulp');
+var sass         = require('gulp-sass');
+var sourcemaps   = require('gulp-sourcemaps');
+var importOnce   = require('node-sass-import-once');
+var cssGlobbing  = require('gulp-css-globbing');
+var autoprefixer = require('gulp-autoprefixer');
+var sassdoc      = require('sassdoc');
 
 // Identify Sass environment
 var nsass = require('gulp-sass/node_modules/node-sass');
 console.log(nsass.info);
 
-// Gulp Sass Task
+// -----------------------------------------------------------------------------
+// Configuration
+// -----------------------------------------------------------------------------
+
+var input = './assets/sass/**/*.{scss,sass}';
+//var input = './assets/sass/{,*/}*.{scss,sass}';
+var output = './public/css';
+var sassdocOptions = { dest: './public/sassdoc' };
+var autoprefixerOptions = { browsers: ['last 2 versions', '> 5%', 'Firefox ESR'] };
+var sassOptions = {
+  includePaths: ['./vendor/compass-mixins/lib'],
+  //outputStyle: 'expanded',
+  sourceMap: true,
+  //sourceMapContents: true,
+  sourceMapEmbed: true,
+  outFile: './css',
+  importer: importOnce,
+  importOnce: {
+    index: false,
+    css: false,
+    bower: true
+  },
+  errLogToConsole: true
+};
+
+
+// -----------------------------------------------------------------------------
+// Sass compilation
+// -----------------------------------------------------------------------------
+
 gulp.task('sass', function() {
-  gulp.src('./sass/*.scss')
+  return gulp
+    .src(input)
     .pipe(cssGlobbing({
       extensions: ['.css', '.scss']
     }))
     //.pipe(sourcemaps.init())
-    .pipe(sass({
-      includePaths: [
-        './node_modules/breakpoint-sass/stylesheets',
-        './node_modules/compass-mixins/lib'
-      ],
-      outputStyle: 'nested',
-      sourceMap: true,
-      sourceMapContents: true,
-      sourceMapEmbed: false,
-      outFile: './css-node',
-      importer: importOnce,
-      importOnce: {
-        index: false,
-        css: false,
-        bower: false
-      },
-      functions: assetFunctions({
-        images_path: './images',
-        fonts_path: './fonts',
-        http_images_path: './images',
-        http_fonts_path: './fonts',
-        asset_cache_buster: function (http_path, real_path, done) {
-          done('1442217566');
-        }
-      }),
-      errLogToConsole: true
-    }))
+    //.pipe(sass(sassOptions).on('error', sass.logError))
+    .pipe(sass(sassOptions))
     //.pipe(sourcemaps.write())
-    .pipe(gulp.dest('./css-node'));
+    .pipe(autoprefixer(autoprefixerOptions))
+    .pipe(gulp.dest(output));
 });
 
-// Create Gulp Default Task
-// ------------------------
-// Having watch within the task ensures that 'sass' has already ran before watching
-//
-// This setup is slightly different from the one on the blog post at
-// http://www.zell-weekeat.com/gulp-libsass-with-susy/#comment-1910185635
-gulp.task('default', ['sass'], function () {
-  gulp.watch('./{,*/}*.{scss,sass}', ['sass'])
+
+// -----------------------------------------------------------------------------
+// Sass documentation generation
+// -----------------------------------------------------------------------------
+
+gulp.task('sassdoc', function () {
+  return gulp
+    .src(input)
+    .pipe(sassdoc(sassdocOptions))
+    .resume();
 });
+
+
+// -----------------------------------------------------------------------------
+// Watchers
+// -----------------------------------------------------------------------------
+
+gulp.task('watch', function() {
+  return gulp
+    // Watch the input folder for change,
+    // and run `sass` task when something happens
+    .watch(input, ['sass'])
+    // When there is a change,
+    // log a message in the console
+    .on('change', function(event) {
+      console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
+});
+
+
+// -----------------------------------------------------------------------------
+// Production build
+// -----------------------------------------------------------------------------
+
+gulp.task('prod', ['sassdoc'], function () {
+  return gulp
+    .src(input)
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(autoprefixer(autoprefixerOptions))
+    .pipe(gulp.dest(output));
+});
+
+
+// -----------------------------------------------------------------------------
+// Default task
+// -----------------------------------------------------------------------------
+
+gulp.task('default', ['sass', 'watch' /*, possible other tasks... */]);
+
